54a1707aaed165fd455f873d9dcc970928e7e508
Parth Sakpal <parthh989@gmail.com>
2026-04-11 14:20:47 +0530

    Update main.java

--- StudentManagementSystem/main.java
+++ StudentManagementSystem/main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class main{
+  public static void main(String[]args){
+  Scanner sc = new Scanner(System.in);
 
+    StudentService st = new StudentService();
+
+    do{
+      System.out.println("----====STUDENT MANAGEMENT SYSTEM====----");
+      System.out.println("1> Add Student");
+      System.out.println("2> View Student");
+      System.out.println("3> Search Student");
+      System.out.println("4> Delete Student");
+      System.out.println("5> EXIT");
+      System.out.print("Enter choice = ");
+      int choice = sc.nextInt();
+
+      switch(choice){
+          
