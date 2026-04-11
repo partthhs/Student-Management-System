ff6959a58f2a7f518dbbfedc69b5dd2877f71e43
Parth Sakpal <parthh989@gmail.com>
2026-04-11 11:29:02 +0530

    Add Student class with properties and display method

--- StudentManagementSystem/Student.java
+++ StudentManagementSystem/Student.java
@@ -1,1 +1,12 @@
+public class Student
+  {
+    int id ;
+    String name;
+    int age;
+    String course;
 
+    void display()
+    {
+      System.out.println("Id : "+id+ ", Name:"+name+", Age :"+age+", Course: "+course);
+    }
+  }
